ce10bbdabe45725874e802b36799464ad6ce0cb9
ZarathustraSama <mr.gabriel@live.it>
2023-08-28 10:39:19 +0200

    Now game and cpu are fused into one class

--- mastermind.rb
+++ mastermind.rb
@@ -3,25 +3,22 @@
 COLORS = %w[red green blue yellow white purple].freeze
 TURNS = 12
 
-# The structure and logic of the game
+# Both the structure and logic of the game and the opponent
 class Mastermind
-  def game_over?(code, code_guess)
-    code_guess == code || TURNS.zero?
-  end
-end
-
-# The opponent of the game
-class Computer
   attr_accessor :code
 
   def initialize
-    @code = [nil, nil, nil, nil]
+    @code = []
   end
 
   def choose_code
-    @code.each_with_index do |_color, index|
-      @code[index] = COLORS.sample
+    0..3.each do
+      @code << COLORS.sample
     end
+  end
+
+  def game_over?(code, code_guess)
+    code_guess == code || TURNS.zero?
   end
 end
 
